dc1b9f100952d49d66e00f927fa2deffde0fb9be
MechaDragonX <sonicraghav@gmail.com>
2019-10-12 21:15:42 -0700

    Tokenize input and combine phrases into a strings without spaces

--- src/com/mechadragonx/recursion/Main.java
+++ src/com/mechadragonx/recursion/Main.java
@@ -1,8 +1,43 @@
 package com.mechadragonx.recursion;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class Main
 {
-    public static void main(String[] args)
+    private static HashSet<String> words = new HashSet<>();
+
+    public static void main(String[] args) throws FileNotFoundException
     {
+        readDictionary(".\\data\\dictionary_words_small.txt");
+    }
+    private static void readDictionary(String path) throws FileNotFoundException
+    {
+        File file = new File(path);
+        Scanner scanner = new Scanner(file);
+        if(file.exists())
+        {
+            while(scanner.hasNextLine())
+            {
+                words.add(scanner.nextLine());
+            }
+        }
+    }
+    private static void findAnagrams(String phrase)
+    {
+        phrase = tokenizeInput(phrase);
+        HashSet<String> found = new HashSet<>();
+    }
+    private static String tokenizeInput(String input)
+    {
+        Scanner wordScan = new Scanner(input);
+        StringBuilder builder = new StringBuilder();
+        while(wordScan.hasNext())
+        {
+            builder.append(wordScan.next().toLowerCase());
+        }
+        return builder.toString();
     }
 }
